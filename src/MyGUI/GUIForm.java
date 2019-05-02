199a5b461274eefc744bd56d9e5c73352630d81e
Wesley <wesleykok-@hotmail.com>
2019-05-03 09:46:31 +1000

    Click event prints coordinates in JPanel

--- src/MyGUI/GUIForm.java
+++ src/MyGUI/GUIForm.java
@@ -2,31 +2,32 @@ package MyGUI;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class GUIForm extends JFrame implements Runnable {
     private JPanel Panel;
-    private JMenuBar MenuBar;
 
     private GUIForm() {
-        setSize(400,400);
+        super("VEC Drawer");
     }
 
     private void createGUI() {
         // Set up window
-        super.setTitle("VEC Drawer");
         this.setPreferredSize(new Dimension(400, 400));
         this.setLocation(new Point(300, 300));
         JMenuBar MenuBar = new JMenuBar();
         this.setJMenuBar(MenuBar);
-
         JMenu file = new JMenu("File");
         JMenuItem exit = new JMenuItem("Exit");
+        JMenuItem open = new JMenuItem("Open File");
+        JMenuItem save = new JMenuItem("Save File");
         MenuBar.add(file);
-
+        file.add(open);
+        file.add(save);
         file.add(exit);
 
+        JMenu help = new JMenu("Help");
+        MenuBar.add(help);
         class exitAction implements ActionListener{
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -36,11 +37,20 @@ public class GUIForm extends JFrame implements Runnable {
 
         exit.addActionListener(new exitAction());
 
-        JMenu help = new JMenu("Help");
+        JPanel panel = new JPanel();
+        this.add(panel, BorderLayout.CENTER);
+        panel.setBackground(Color.BLACK); // black so that it's obvious for now
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println(e);
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                System.out.println(e);
+            }
+        });
 
-        MenuBar.add(help);
-
-        this.setContentPane(new GUIForm().Panel);
         this.pack();
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
