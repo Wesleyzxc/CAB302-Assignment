c76376bc45cd53aca1d213ada9bf3cc1f934a12b
Daryl <dtjh@live.com.sg>
2019-05-22 14:47:14 +1000

    Revert "Save reflects choosing history"
    This reverts commit 46694eb459665666282f7b87bc4871714afe05de.

--- src/MyGUI/SaveAction.java
+++ src/MyGUI/SaveAction.java
@@ -21,6 +21,7 @@ public class SaveAction implements ActionListener {
     }
     @Override
     public void actionPerformed (ActionEvent e)  {
+        String filename = "";
         JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
         fileChooser.setDialogTitle("Choose a directory to save your file: ");
         fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
@@ -28,47 +29,47 @@ public class SaveAction implements ActionListener {
         int returnValue = fileChooser.showSaveDialog(null);
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             if (fileChooser.getSelectedFile().isDirectory()) {
-                String filename = fileChooser.getSelectedFile().toString();
-                VEC = panel.getAllVEC();
-
-                try {
-                    FileWriter writer = new FileWriter(new File(filename + "/output.VEC"));
-                    for(String str: VEC) {
-                        if (panel.getHistory().get(VEC.indexOf(str)).isVisible()){
-                            String[] s = str.split(",");
-                            for (String str2: s) {
-                                writer.write(str2);
-                                writer.write(System.getProperty("line.separator"));
-                            }
-                        }
-                    }
-                    writer.close();
-                    System.out.println("file successfully saved");
-                } catch(IOException a) {
-                    System.out.println("failed to save VEC");
-                }
-
-                panel.setSize(new Dimension(4096,4096));
-                System.out.println(panel.getWidth());
-                BufferedImage bImg = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
-                Graphics2D cg = bImg.createGraphics();
-                panel.paintAll(cg);
-
-                //Save as bmp
-                try {
-                    if (ImageIO.write(bImg, "bmp", new File(filename + "/output.bmp")))
-                    {
-                        System.out.println("-- saved");
-                    }
-                } catch (IOException a) {
-                    System.out.println("failed to save BMP");
-                    a.printStackTrace();
-                }
+                filename = fileChooser.getSelectedFile().toString();
             }
         }
 
+        VEC = panel.getAllVEC();
 
+        try {
+            FileWriter writer = new FileWriter(new File(filename + "/output.VEC"));
+            for(String str: VEC) {
+                if (panel.getHistory().get(VEC.indexOf(str)).isVisible()){
+                    String[] s = str.split(",");
+                    for (String str2: s) {
+                        writer.write(str2);
+                        writer.write(System.getProperty("line.separator"));
+                }
+                }
 
+            }
+            writer.close();
+            System.out.println("file successfully saved");
+        } catch(IOException a) {
+            System.out.println("failed to save VEC");
+        }
+
+        panel.setSize(new Dimension(4096,4096));
+        System.out.println(panel.getWidth());
+        BufferedImage bImg = new BufferedImage(panel.getWidth(), panel.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D cg = bImg.createGraphics();
+        panel.paintAll(cg);
+
+        //Save as bmp
+        try {
+            if (ImageIO.write(bImg, "bmp", new File(filename + "/output.bmp")))
+            {
+                System.out.println("-- saved");
+            }
+        } catch (IOException a) {
+            // TODO Auto-generated catch block
+            System.out.println("failed to save BMP");
+            a.printStackTrace();
+        }
 
 
     }
